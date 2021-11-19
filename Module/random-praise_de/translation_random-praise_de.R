fc3728cd2a6e6e036edf7e1dadb1508b630d4aae
Griesenbeck <tabea.griesenbeck@bcw-gruppe.de>
2021-11-19 09:35:55 +0100

    Umlaute wieder eingefügt

--- Module/random-praise_de/translation_random-praise_de.R
+++ Module/random-praise_de/translation_random-praise_de.R
@@ -1,7 +1,7 @@
 random_praises <- c(
   "Absolut genial!",
   "Herausragend!",
-  "Sehr schoen!",
+  "Sehr schön!",
   "Bravo!",
   "Exzellent!",
   "Cool!",
@@ -12,17 +12,17 @@ random_praises <- c(
   "Da weiß jemand was er tut!",
   "Da kennt sich jemand aus!",
   "Gute Arbeit!",
-  "Spektakulaer!",
+  "Spektakulär!",
   "Klasse!",
   "Wundervoll!",
   "Spitze!",
-  "Praechtig!",
+  "Prächtig!",
   "Erfolg!",
   "Unglaubliche Arbeit!",
   "Das ist eine Eins-a-Antwort!",
   "Das ist glorreich!",
   "Das ist ein Ass!",
-  "Sie koennen stolz auf sich sein!",
+  "Sie können stolz auf sich sein!",
   "Besser geht's nicht!",
   "Perfekt!",
   ":-)")
@@ -30,7 +30,7 @@ random_praises <- c(
 random_encouragements <- c(
   "Versuchen Sie es noch einmal!",
   "Geben Sie nicht auf, versuchen Sie es noch einmal!",
-  "Versuchen Sie es noch einmal. Ausdauer ist der Schluessel zum Erfolg!",
+  "Versuchen Sie es noch einmal. Ausdauer ist der Schlüssel zum Erfolg!",
   "Kein Ding: Wir lernen mehr aus Fehlern als aus Erfolgen. Lassen Sie es uns noch einmal versuchen!",
   "Nicht verzagen! Uebung macht den Meister!")
 
